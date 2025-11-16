cd030ca5afba6a8865ec9a50568a978c31ac716b
Petssampa <petssampa@gmail.com>
2025-11-16 11:09:29 -0300

    Add route for BlogPostExemplo in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import BlogPostExemplo from "./pages/BlogPostExemplo";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -37,6 +37,7 @@ const App = () => (
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog/mainecoon" element={<MaineCoonPost />} /> // 👈 NOVA LINHA
           <Route path="/blog/acupuntura-dor-lombar" element={<BlogPostDorLombar />} />
+          <Route path="/blog/acupuntura-ansiedade" element={<BlogPostExemplo />} />
           <Route path="/contato" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
