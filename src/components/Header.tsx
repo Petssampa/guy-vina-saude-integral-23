c62d989854c20110325d19db61d581c893bd5c24
Petssampa <petssampa@gmail.com>
2025-11-28 22:12:23 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -97,7 +97,7 @@ const Header = () => {
                 Home
               </Link>
               <Link 
-                to="/sobre-guy-vina-medicina-chinesa" 
+                to="/sobre-geisler-viana" 
                 className="transition-colors duration-300 py-2"
                 style={{ color: '#174288' }}
                 onMouseEnter={(e) => (e.target as HTMLElement).style.color = '#a87a47'}
